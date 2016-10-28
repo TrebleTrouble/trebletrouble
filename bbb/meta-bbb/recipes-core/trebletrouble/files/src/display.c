d333bf9c15508bd73597e2d8d88b08a0a2dbf05a
Will <uiri00@gmail.com>
2016-10-27 20:02:21 -0400

    Add code to blit images onto the screen.

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/display.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/display.c
@@ -7,8 +7,67 @@
 #include <linux/fb.h>
 #include <sys/mman.h>
 
+int DISP_WIDTH = 1;
+
+int bitblit(char* filename, char* fbp, int screensize, int x, int y) { /*, int w, int h) {*/
+	FILE* fd;
+	int i, j, w, h;
+	unsigned char buf[3], px[2];
+
+	fd = fopen(filename, "rb");
+	if (fd == NULL)
+		return -1;
+	fread(buf, 1, 3, fd);
+	if (buf[0] != 'P' || buf[1] != '6')
+		return -2;
+	fread(buf, 1, 3, fd);
+	w = 0;
+	for (i = 0; i < 3; i++) {
+		if (buf[i] == ' ' || (9 < buf[i] && buf[i] < 13))
+			break;
+		w *= 10;
+		w += buf[i] - '0';
+	}
+	if (i == 3) {
+		/* throw away whitespace */
+		fread(buf, 1, 1, fd);
+	} else if (i < 2) {
+		for (j = 0; j+i+1 < 3; i++) {
+			buf[j] = buf[j+i+1];
+		}
+	}
+	fread(buf, 1, 3, fd);
+	h = 0;
+	for (i = 0; i < 3; i++) {
+		if (buf[i] == ' ' || (9 < buf[i] && buf[i] < 13))
+			break;
+		h *= 10;
+		h += buf[i] - '0';
+	}
+	if (i == 3) {
+		buf[0] = fgetc(fd);
+	}
+	do {
+		buf[0] = fgetc(fd);
+	} while (buf[0] != ' ' && (buf[0] < 9 || 13 < buf[0]));
+	for (i = 0; i < screensize/2; i++) {
+		if ((i / DISP_WIDTH) < y || (y+h) < (i / DISP_WIDTH))
+			continue;
+		if ((i % DISP_WIDTH) < x || (x+w) < (i % DISP_WIDTH))
+			continue;
+		if (fread(buf, 1, 3, fd) != 3) {
+			break;
+		}
+		px[0] = (buf[0] & 0xF8) | ((buf[1] & 0xE0) >> 5);
+		px[1] = ((buf[1] & 0x1C) << 3) | ((buf[2] & 0xF8) >> 3);
+		memset(fbp + 2*i, *(short*)px, 2);
+	}
+
+	return 0;
+}
+
 int display(void) {
-	int fbfd, i, xpos;
+	int fbfd, i, xpos, err;
 	struct fb_var_screeninfo vinfo;
 	struct fb_fix_screeninfo finfo;
 	long screensize;
@@ -28,6 +87,7 @@ int display(void) {
 		printf("Error reading fixed screen info\n");
 	}
 
+	DISP_WIDTH = vinfo.xres;
 	screensize = finfo.smem_len;
 
 	fbp = (char*)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, 
@@ -49,6 +109,16 @@ int display(void) {
 		 * Red - 1110 0; Green - 010 001; Blue - 0 0101)
 		 */
 		memset(fbp + i, 0x25E2, 2);
+	}
+
+	err = bitblit("/srv/trebletrouble/timbit.pnm", fbp, screensize, 200, 120);
+
+	if (err) {
+		if (err == -1) {
+			printf("File not found :(");
+		} else if (err == -2) {
+			printf("Not P6 file");
+		}
 	}
 
 	// fuck it, loop forever
