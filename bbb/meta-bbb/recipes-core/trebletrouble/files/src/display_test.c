c6330160b5de13e9189823c2945269eb82ce7bca
Emilie <ecobb042@uottawa.ca>
2016-11-24 15:42:44 +0000

    Added the staff to display_test Added the staff, end lines and clef for testing the notes.

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/display_test.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/display_test.c
@@ -8,6 +8,9 @@
 extern long SCREENSIZE;
 extern int DISP_WIDTH;
 
+char *N[12] = {"a", "as", "b", "c", "cs", "d", "ds", "e", "f", "fs", "g", "gs"};
+int O[9] = {0,1,2,3,4,5,6,7,8};
+
 void screen_capture(char* fbp, char* filename){
 
 	FILE* file;
@@ -15,6 +18,7 @@ void screen_capture(char* fbp, char* filename){
 	
 	
 	char* e = fbp+800*480*2;
+	
 	//creating buf for r -> buf[0], g -> buf[1] & buf[2], b -> buf[3]
 	unsigned char buf[3], px[2];
 	
@@ -54,21 +58,35 @@ void screen_capture(char* fbp, char* filename){
 
 int main(int argc, char** argv){
 
+	int i;
 	//set screensize and display_width for testing
 	SCREENSIZE = 800*480*2;
 	DISP_WIDTH = 800;
 
 	//Char for testing fbp
 	char* fbp = malloc(SCREENSIZE);
-	int tims;
 	
-	//test colour screen
-	colour_screen(fbp, ORANGE);	
+	//draw colour to screen
+	colour_screen(fbp, WHITE);	
 
 
-	//write timbits to screen
-	tims = bitblit("/srv/trebletrouble/timbit.pnm", fbp, 400, 240);
+	//place staff (five lines) on screen 
+	int x = 0;  //stop changing me
+	int y = 95; 
+	int m = 40; //line space
+	int start =0;
+	int end = 800;
 	
+	//draw clef and staff
+	bitblit("/srv/trebletrouble/clef.pnm", fbp, start+10, start+66);	
+	bitblit("/srv/trebletrouble/end.pnm", fbp, end-25, y+5);	
+	bitblit("/srv/trebletrouble/end.pnm", fbp, end-10, y+5);	
+
+	
+	for(i=0; i < 5; i++){
+		bitblit ("/srv/trebletrouble/line.pnm", fbp, x, y);
+		y = y+m;
+	}
 	
 	//writes the thing to a pnm hopefully
 	screen_capture(fbp, "screenshot.pnm");
