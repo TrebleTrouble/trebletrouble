9886543260db8ead7a6653952b36b4cb9d826c55
Emilie <ecobb042@uottawa.ca>
2016-11-25 03:06:34 +0000

    Added ability to display notes on staff given a file with notes
    display_test can now call draw_notes to draw notes on a single staff.
    
    There's fancy math to locate the y variable, and it works!
    
    The scope of the notes that are able to be shown has been limited to C2 to C6.
    
    Also added needed/updated pnm files: clef.pnm, end.pnm, line.pnm, and note.pnm.

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/display_test.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/display_test.c
@@ -11,11 +11,14 @@ extern int DISP_WIDTH;
 char *N[12] = {"a", "as", "b", "c", "cs", "d", "ds", "e", "f", "fs", "g", "gs"};
 int O[9] = {0,1,2,3,4,5,6,7,8};
 
+
+char *NOTE_PIC = "/srv/trebletrouble/note.pnm";
+char *L_PIC = "/srv/trebletrouble/note_l.pnm";
+char *SONG = "/srv/trebletrouble/twinkle_v1"; 
+
 void screen_capture(char* fbp, char* filename){
 
 	FILE* file;
-
-	
 	
 	char* e = fbp+800*480*2;
 	
@@ -50,13 +53,41 @@ void screen_capture(char* fbp, char* filename){
 		fwrite(buf, 1, 3, file);
 
 	}
-	
 
 	fclose(file);
+}
+
+void display_note_test(double freq){
+	int ind;
+	char *note;
+	int oct;
+
+	ind = find_freq(freq);
+
+	note = N[ind % 12];
+	oct = O[(ind+10) /12];
+
+	//print out data for testing
+	printf("freq: %f, ind: %d, note: %s, octave:%d\n", freq, ind, note, oct);
+	
+	//bitblit(NOTE_PIC)
 
 }
 
+void draw_note(unsigned char *buf, int xnote, char* fbp){
+	int ynote;
+	printf("buf:%s\n", buf);
+	/*Middle C = 240*/
+	/* find the y position for each note*/
+	ynote =(240 - (((buf[1]-'4') * 105) + (((buf[0]-'C'+7)%7)*15)));
+
+	bitblit(NOTE_PIC, fbp, xnote, ynote-15);	
+}
+
 int main(int argc, char** argv){
+
+//	double freq = 523.251; //Tenor C
+	
 
 	int i;
 	//set screensize and display_width for testing
@@ -72,22 +103,41 @@ int main(int argc, char** argv){
 
 	//place staff (five lines) on screen 
 	int x = 0;  //stop changing me
-	int y = 95; 
-	int m = 40; //line space
+	int y = 90; 
+	int m = 30; //line space
 	int start =0;
 	int end = 800;
-	
-	//draw clef and staff
-	bitblit("/srv/trebletrouble/clef.pnm", fbp, start+10, start+66);	
-	bitblit("/srv/trebletrouble/end.pnm", fbp, end-25, y+5);	
-	bitblit("/srv/trebletrouble/end.pnm", fbp, end-10, y+5);	
+	int xstart = 145;
+	int xnote = xstart;
+	FILE *song = fopen(SONG, "rb");
+	unsigned char buf[2];
 
-	
+	//draw clef and end lines
+	bitblit("/srv/trebletrouble/clef.pnm", fbp, start, y-35);	
+	bitblit("/srv/trebletrouble/end.pnm", fbp, end-15, y);	
+	bitblit("/srv/trebletrouble/end.pnm", fbp, end-5, y);	
+
+	//draw the lines of the staff	
 	for(i=0; i < 5; i++){
 		bitblit ("/srv/trebletrouble/line.pnm", fbp, x, y);
 		y = y+m;
 	}
 	
+	//draw the notes
+	while (fread(buf, 1, 2, song) == 2){
+		
+		draw_note(buf, xnote, fbp);
+		xnote = xnote+m+4;
+		printf("it works?\n");
+
+	}
+	fclose(song);
+
+	//place note on the board	
+	/*while (xnote=0;xnote<15 ;xnote++ ){
+		display_note_test(*notes*, xnote);	
+	}*/
+
 	//writes the thing to a pnm hopefully
 	screen_capture(fbp, "screenshot.pnm");
 	
