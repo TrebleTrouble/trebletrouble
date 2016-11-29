75df3ae3852329d352091e9a93ee1f7d98498c04
daniel <drosa098@uottawa.ca>
2016-11-29 23:44:50 +0000

    Managed to make a wav file play once using Alsa *Changes to hw_alsa function parameters so that the wav file woud be read properly

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/simpleAlsa.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/simpleAlsa.c
@@ -24,8 +24,83 @@
 #include <alsa/asoundlib.h>
 #include <stdio.h>
 #include <sndfile.h>
+#include <math.h>
 
-#define PCM_DEVICE "default"
+#define PCM_DEVICE "plughw:0,0"
+
+static double freq = 440;                               /* sinusoidal wave frequency in Hz */
+static int verbose = 0;                                 /* verbose flag */
+static int resample = 1;                                /* enable alsa-lib resampling */
+static int period_event = 0;                            /* produce poll event after each period */
+static unsigned int rate = 44100;                       /* stream rate */
+static unsigned int channels = 1;                       /* count of channels */
+static snd_pcm_format_t format = SND_PCM_FORMAT_S16;    /* sample format */
+
+
+static void generate_sine(const snd_pcm_channel_area_t *areas, 
+                          snd_pcm_uframes_t offset,
+                          int count, double *_phase)
+{
+        static double max_phase = 2. * M_PI;
+        double phase = *_phase;
+        double step = max_phase*freq/(double)rate;
+        unsigned char *samples[channels];
+        int steps[channels];
+        unsigned int chn;
+        int format_bits = snd_pcm_format_width(format);
+        unsigned int maxval = (1 << (format_bits - 1)) - 1;
+        int bps = format_bits / 8;  /* bytes per sample */
+        int phys_bps = snd_pcm_format_physical_width(format) / 8;
+        int big_endian = snd_pcm_format_big_endian(format) == 1;
+        int to_unsigned = snd_pcm_format_unsigned(format) == 1;
+        int is_float = (format == SND_PCM_FORMAT_FLOAT_LE ||
+                        format == SND_PCM_FORMAT_FLOAT_BE);
+        /* verify and prepare the contents of areas */
+        for (chn = 0; chn < channels; chn++) {
+                if ((areas[chn].first % 8) != 0) {
+                        printf("areas[%i].first == %i, aborting...\n", chn, areas[chn].first);
+                        exit(EXIT_FAILURE);
+                }
+                samples[chn] = /*(signed short *)*/(((unsigned char *)areas[chn].addr) + (areas[chn].first / 8));
+                if ((areas[chn].step % 16) != 0) {
+                        printf("areas[%i].step == %i, aborting...\n", chn, areas[chn].step);
+                        exit(EXIT_FAILURE);
+                }
+                steps[chn] = areas[chn].step / 8;
+                samples[chn] += offset * steps[chn];
+        }
+        /* fill the channel areas */
+        while (count-- > 0) {
+                union {
+                        float f;
+                        int i;
+                } fval;
+                int res, i;
+                if (is_float) {
+                        fval.f = sin(phase);
+                        res = fval.i;
+                } else
+                        res = sin(phase) * maxval;
+                if (to_unsigned)
+                        res ^= 1U << (format_bits - 1);
+                for (chn = 0; chn < channels; chn++) {
+                        /* Generate data in native endian format */
+                        if (big_endian) {
+                                for (i = 0; i < bps; i++)
+                                        *(samples[chn] + phys_bps - 1 - i) = (res >> i * 8) & 0xff;
+                        } else {
+                                for (i = 0; i < bps; i++)
+                                        *(samples[chn] + i) = (res >>  i * 8) & 0xff;
+                        }
+                        samples[chn] += steps[chn];
+                }
+                phase += step;
+                if (phase >= max_phase)
+                        phase -= max_phase;
+        }
+        *_phase = phase;
+}
+
 
 int sound (void){
 
@@ -57,9 +132,9 @@ int sound (void){
         fprintf(stderr,"Sections: %d\n", sfinfo.sections);
 	        fprintf(stderr,"Format: %d\n", sfinfo.format);
 
-		rate = 44100;
-	channels = 1;
-	seconds = 0.5;
+		//	rate = 44100;
+		//channels = 1;
+		//seconds = 0.5;
 
 	/* Open the PCM device in playback mode */
 	if (pcm = snd_pcm_open(&pcm_handle, PCM_DEVICE,
@@ -74,14 +149,14 @@ int sound (void){
 	if (pcm = snd_pcm_hw_params_set_access(pcm_handle, params,
 					SND_PCM_ACCESS_RW_INTERLEAVED) < 0) 
 		printf("ERROR: Can't set noninterleaved mode. %s\n", snd_strerror(pcm));
-
+	//set formatting 
 	if (pcm = snd_pcm_hw_params_set_format(pcm_handle, params,
 						SND_PCM_FORMAT_S16_LE) < 0) 
 		printf("ERROR: Can't set format. %s\n", snd_strerror(pcm));
 	//Setting channels
-	if (pcm = snd_pcm_hw_params_set_channels(pcm_handle, params,sfinfo.channels) < 0) 
+	if (pcm = snd_pcm_hw_params_set_channels(pcm_handle, params,1) < 0) 
 		printf("ERROR: Can't set channels number. %s\n", snd_strerror(pcm));
-
+	//Set sample rate
 	if (pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &sfinfo.samplerate, 0) < 0) 
 		printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
 	
@@ -113,7 +188,7 @@ int sound (void){
     fprintf(stderr,"Starting read/write loop\n");
     buf = malloc(frames * sfinfo.channels * sizeof(short));
     while ((readcount = sf_readf_short(infile, buf, frames))>0) {
-      printf("My readcount is:%d",readcount);
+	    // printf("My readcount is:%d",readcount);
         pcmrc = snd_pcm_writei(pcm_handle, buf, frames);
         if (pcmrc == -EPIPE) {
             fprintf(stderr, "Underrun!\n");
@@ -135,7 +210,7 @@ int sound (void){
     //Close the device
     snd_pcm_close(pcm_handle);
     free(buf);
-
+    printf("I freed the buf and closed the device");
     return 0;
 
 				
