7f67c1a7ab614ba9cbc0240f292e2bdb159ccb04
Will <uiri00@gmail.com>
2017-02-16 12:29:54 -0500

    Buttons!
    Configured the framebuffer driver.
    GUI.c contents show up.
    Still need to hook them up to stuff.
    Fix includes/library in trebletrouble application Makefile

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/app.c
@@ -27,37 +27,53 @@
  *
  */
 
-#include "colours.h"
-#include "display.h"
-#include "input.h"
-#include "menu.h"
+/* #include "colours.h" */
+/* #include "display.h" */
+/* #include "input.h" */
+/* #include "menu.h" */
+
+#include "gfx.h"
+#include "gui.h"
 
 int main(int argc, char** argv) {
-	char* fbp;
-	int err;
-	int fbfd;
-	ScreenInput si;
-	void (*menu_item)(char* fbp, ScreenInput* si);
+	/* char* fbp; */
+	/* int err; */
+	/* int fbfd; */
+	/* ScreenInput si; */
+	/* void (*menu_item)(char* fbp, ScreenInput* si); */
 
-	fbp = init_display(&fbfd);
+	gfxInit();
 
-	err = init_touchscreen(&si.fd);
-	if (err) {
-		printf("Error reading event input file\n");
-		return err;
-	}
+	gdispSetBacklight(100);
+	gdispSetContrast(100);
 
-	colour_screen(fbp, ORANGE);
-	get_lcd_input(&si);
+	geventListenerInit(&glistener);
+	gwinAttachListener(&glistener);
 
-	if (si.y > (TS_MAX_Y / 2)) {
-		menu_item = metronome_menu;
-	} else {
-		menu_item = play_song_menu;
-	}
-	menu_item(fbp, &si);
+	guiCreate();
 
-	get_lcd_input(&si);
-	cleanup_display(fbp, &fbfd);
+	while (1)
+		guiEventLoop();
+
+	/* fbp = init_display(&fbfd); */
+
+	/* err = init_touchscreen(&si.fd); */
+	/* if (err) { */
+	/* 	printf("Error reading event input file\n"); */
+	/* 	return err; */
+	/* } */
+
+	/* colour_screen(fbp, ORANGE); */
+	/* get_lcd_input(&si); */
+
+	/* if (si.y > (TS_MAX_Y / 2)) { */
+	/* 	menu_item = metronome_menu; */
+	/* } else { */
+	/* 	menu_item = play_song_menu; */
+	/* } */
+	/* menu_item(fbp, &si); */
+
+	/* get_lcd_input(&si); */
+	/* cleanup_display(fbp, &fbfd); */
 	return 0;
 }
