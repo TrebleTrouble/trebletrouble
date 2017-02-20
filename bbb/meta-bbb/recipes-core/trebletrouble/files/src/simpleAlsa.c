923a378602966b1586dd7ef7d76bcc2b9af004bd
Will <uiri00@gmail.com>
2017-02-20 14:40:23 -0500

    Format simpleAlsa. Change from DOS to Unix line endings

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/simpleAlsa.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/simpleAlsa.c
@@ -39,7 +39,7 @@
  *
  *
  *
- * 
+ *
  */
 
 #include <alsa/asoundlib.h>
@@ -51,97 +51,100 @@
 #define PCM_DEVICE "default"
 /* WAV code */
 static snd_pcm_uframes_t frames;
-static SNDFILE *infile = NULL;	
+static SNDFILE *infile = NULL;
 
 /* added to fix implicit declaration warnings for alloca in target files */
 #ifndef alloca
 #define alloca(x) __builtin_alloca(x)
 #endif
 
-snd_pcm_t * init_pcm(char *infilename,short **buf){
-        
+snd_pcm_t * init_pcm(char *infilename,short **buf)
+{
 	SF_INFO sfinfo;
 	snd_pcm_hw_params_t *params;
 	snd_pcm_t *pcm_handle;
 	int dir;
 	unsigned int tmp,pcm;
 	dir = 0;
-	if ( access( infilename, F_OK) ==-1){
-		printf("File doesn't exists");	
-	}
-	else{
-	  /*	printf("File does exist!"); */
+	if ( access( infilename, F_OK) ==-1) {
+		printf("File doesn't exists");
+	} else{
+		/* printf("File does exist!"); */
 	}
 	infile = sf_open(infilename, SFM_READ, &sfinfo);
         /* Open the PCM device in playback mode */
 	if ((pcm = snd_pcm_open(&pcm_handle, PCM_DEVICE,
-				SND_PCM_STREAM_PLAYBACK, 0)) < 0) 
-	  	printf("ERROR: Can't open \"%s\" PCM device. %s\n",
-					PCM_DEVICE, snd_strerror(pcm));
-       	/* Allocate parameters object and fill it with default values*/
+				SND_PCM_STREAM_PLAYBACK, 0)) < 0)
+		printf("ERROR: Can't open \"%s\" PCM device. %s\n",
+		       PCM_DEVICE, snd_strerror(pcm));
+	/* Allocate parameters object and fill it with default values*/
 	snd_pcm_hw_params_alloca(&params);
 	snd_pcm_hw_params_any(pcm_handle, params);
 	/* Set parameters */
-	if ((pcm = snd_pcm_hw_params_set_access(pcm_handle, params,
-						SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) 
-		printf("ERROR: Can't set noninterleaved mode. %s\n", snd_strerror(pcm));
+	pcm = snd_pcm_hw_params_set_access(pcm_handle, params,
+					   SND_PCM_ACCESS_RW_INTERLEAVED);
+	if (pcm < 0)
+		printf("ERROR: Can't set noninterleaved mode. %s\n",
+		       snd_strerror(pcm));
 	/* Set formatting */
-	if ((pcm = snd_pcm_hw_params_set_format(pcm_handle, params,
-					       SND_PCM_FORMAT_S16_LE)) < 0) 
+	pcm = snd_pcm_hw_params_set_format(pcm_handle, params,
+					   SND_PCM_FORMAT_S16_LE);
+	if (pcm < 0)
 		printf("ERROR: Can't set format. %s\n", snd_strerror(pcm));
+
 	/* Setting channels */
-	if ((pcm = snd_pcm_hw_params_set_channels(pcm_handle, params,1)) < 0) 
-		printf("ERROR: Can't set channels number. %s\n", snd_strerror(pcm));
+	pcm = snd_pcm_hw_params_set_channels(pcm_handle, params,1);
+	if (pcm < 0)
+		printf("ERROR: Can't set channels number. %s\n",
+		       snd_strerror(pcm));
+
 	/* Set sample rate */
-	if ((pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, (unsigned int*)&sfinfo.samplerate, &dir)) < 0) 
+	pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params,
+					      (unsigned int*)&sfinfo.samplerate,
+					      &dir);
+	if (pcm < 0)
 		printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
-	
-		
 
 	/* Resume information */
-       	snd_pcm_hw_params_get_channels(params, &tmp);
+	snd_pcm_hw_params_get_channels(params, &tmp);
 	if (tmp == 1)
 		printf("(mono)\n");
 	else if (tmp == 2)
 		printf("(stereo)\n");
 	snd_pcm_hw_params_get_rate(params, &tmp, 0);
-       	/* Write parameters */
+	/* Write parameters */
 	snd_pcm_hw_params(pcm_handle, params);
 	/* Allocate buffer to hold single period */
 	snd_pcm_hw_params_get_period_size(params, &frames, &dir);
 	*buf = malloc(frames * sfinfo.channels * sizeof(short));
 	return pcm_handle;
 }
-void cleanup_pcm(snd_pcm_t *pcm_handle,short *buf){
-    /*Drain the buffer*/
-    snd_pcm_drain(pcm_handle);
-    /*Close the device*/
-    snd_pcm_close(pcm_handle);
-    free(buf);
-    
 
+void cleanup_pcm(snd_pcm_t *pcm_handle,short *buf)
+{
+	/*Drain the buffer*/
+	snd_pcm_drain(pcm_handle);
+	/*Close the device*/
+	snd_pcm_close(pcm_handle);
+	free(buf);
 }
 
-
-
-void sound (snd_pcm_t *pcm_handle,short *buf){
-
+void sound (snd_pcm_t *pcm_handle,short *buf)
+{
 	int pcmrc;
 	short readcount;
-    while ((readcount = sf_readf_short(infile, buf, frames))>0) {
-	
-        pcmrc = snd_pcm_writei(pcm_handle, buf, frames);
-        if (pcmrc == -EPIPE) {
-	  /*    fprintf(stderr, "Underrun!\n"); */
-            snd_pcm_prepare(pcm_handle);
-	        }
-        else if (pcmrc < 0) {
-	  /*            fprintf(stderr, "Error writing to PCM device: %s\n", snd_strerror(pcmrc)); */
-        }
-        else if (pcmrc != readcount) {
-	  /*    fprintf(stderr,"PCM write differs from PCM read.\n"); */
-        }
-
-    }
-    sf_seek(infile,0,SEEK_SET);
-    }
+	while ((readcount = sf_readf_short(infile, buf, frames))>0) {
+		pcmrc = snd_pcm_writei(pcm_handle, buf, frames);
+		if (pcmrc == -EPIPE) {
+			/* fprintf(stderr, "Underrun!\n"); */
+			snd_pcm_prepare(pcm_handle);
+		} else if (pcmrc < 0) {
+			/* fprintf(stderr, "Error writing to PCM device: %s\n",
+			   snd_strerror(pcmrc)); */
+		} else if (pcmrc != readcount) {
+			/* fprintf(stderr,"PCM write differs from PCM read.\n");
+			 */
+		}
+	}
+	sf_seek(infile,0,SEEK_SET);
+}
