793612d02be9a80a748d80b48ae41554d10a0f73
Emilie Cobbold <ecobb042@uottawa.ca>
2017-03-23 22:39:13 -0400

    Update fileformat to test multiple notes

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/fileFormat.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/fileFormat.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "fileFormat.h"
 
+#define NUMBAR = 8;
+#define NUMNOTES = 28;
+
 void makeBin(char * filename)
 {
 	int i;
@@ -8,8 +11,8 @@ void makeBin(char * filename)
 	unsigned char ts = 51;
 	SongFormatHeader sfh = {100,4,14,C_MAJ,ts,"twinkle twinkle little star"};
 	//4 bars of twinkle twinkle 
-	Bar allBars[4];
-	Note allNotes [14];
+	Bar allBars[NUMBAR];
+	Note allNotes [NUMNOTES];
 	
 	i=0;
 	j=0;
@@ -102,10 +105,118 @@ void makeBin(char * filename)
 	allNotes[i].duration = millisec(sfh.bpm, ts, MINIM);
 	allNotes[i].dotted = 0; 
 	i++;
+
+	//Fifth Bar - start over
+	allNotes[i].quantumvalue = CROTCHET; 
+    allNotes[i].frequency =392.628; 
+    allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+                	
+    allNotes[i].quantumvalue = CROTCHET; 
+    allNotes[i].frequency =392.628; 
+    allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+    allNotes[i].dotted = 0; 
+	i++;
+
+
+	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =261.626; 
+    allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+    allNotes[i].dotted = 0; 
+	i++;
+        
+    allNotes[i].quantumvalue = CROTCHET; 
+    allNotes[i].frequency =261.626; 
+    allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+    allNotes[i].dotted = 0; 
+	i++;
+                	
+
+        	
+	//Sixth Bar
+	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =440.0; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+        
+	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =440.0; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+        
+	allNotes[i].quantumvalue = MINIM; 
+	allNotes[i].frequency =415.305; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, MINIM);
+	allNotes[i].dotted = 0; 
+	i++;
+	
+	//Seventh Bar
+
+	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =349.228; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+        
+       	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =349.228; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+        
+	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =329.628; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+
+	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =329.628; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+	
+	//Eighth Bar
+	
+	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =293.665; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+		
+	allNotes[i].quantumvalue = CROTCHET; 
+	allNotes[i].frequency =293.665; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, CROTCHET);
+	allNotes[i].dotted = 0; 
+	i++;
+
+	allNotes[i].quantumvalue = MINIM; 
+	allNotes[i].frequency =261.626; 
+	allNotes[i].duration = millisec(sfh.bpm, ts, MINIM);
+	allNotes[i].dotted = 0; 
+	i++;
 	
 	//Keep the starting point of each note stored in a bar	
 	
 	
+
+	allBars[j].notes =4;
+	j++;
+	
+
+	allBars[j].notes =3;
+	j++;
+
+
+	allBars[j].notes =4;
+	j++;
+
+
+	allBars[j].notes =3;
+	j++
 
 	allBars[j].notes =4;
 	j++;
@@ -125,8 +236,8 @@ void makeBin(char * filename)
 	/* Create the file */
 	FILE *fh = fopen (filename, "wb+");
 	fwrite(&sfh,sizeof(SongFormatHeader),1,fh);
-	fwrite (allBars, sizeof (Bar), 4, fh);
-        fwrite(allNotes,sizeof(Note),14,fh);
+	fwrite (allBars, sizeof (Bar), NUMBAR, fh);
+        fwrite(allNotes,sizeof(Note), NUMNOTES,fh);
 	fclose(fh);
 }
 
