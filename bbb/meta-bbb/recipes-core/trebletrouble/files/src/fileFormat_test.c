7e80003d5047caf6f3781bb8585eb25b36393ac1
daniel <drosa098@uottawa.ca>
2017-03-24 15:31:17 +0000

    Fix indentation, comments , and unused variables

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/fileFormat_test.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/fileFormat_test.c
@@ -9,27 +9,18 @@
 int main(int argc, char** argv)
 {
 	Song * ttls;
-	char tname [50] ;
 	int i;
 	ttls =(Song *)malloc (sizeof(ttls));
 	Note * notes;
 	char binname[80];
 	strcpy(tname,argv[1]);
-	printf("This is what you passes:%s",tname);
 	makeBin(argv[1],binname);
 	notes=readTwinkle(ttls,binname);
 	printHeader(ttls);
 	for(i= 0;i<ttls->sfh->numNotes;i++){
-	printNote(notes);
-	notes++;
+		printNote(notes);
+		notes++;
 	}
-	//printNote(notes);
-	//notes++;
-	//printNote(notes);
-	//notes++;
-	//printNote(notes);
-	//notes++;
-	//parseBar(ttls,notes);
 	freeSong(ttls);
 	return 1;
 }
