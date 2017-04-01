9222c7e5093c6383b8679e1d38aa781a3f84d3a1
Will <uiri00@gmail.com>
2017-04-01 13:40:38 -0400

    Update LICENSE headers in source files

--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/find_freq.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/find_freq.c
@@ -1,3 +1,31 @@
+/*
+ *  Copyright (C) 2016  William Pearson
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7
+ *
+ *  If you modify this Program, or any covered work, by linking or
+ *  combining it with the uGFX Library, containing parts covered by the
+ *  terms of the GFX License, Version 1.2, the licensors of this Program
+ *  grant you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of the uGFX Library used
+ *  as well as that of the covered work.
+ *
+ */
+
 #define KEYS 88
 
 double FREQS[KEYS] = {27.5, 29.1352, 30.8677, 32.7032, 34.6478, 36.7081, 38.8909, 41.2034, 43.6535, 46.2493, 48.9994, 51.9131, 55.0, 58.2705, 61.7354, 65.4064, 69.2957, 73.4162, 77.7817, 82.4069, 87.3071, 92.4986, 97.9989, 103.826, 110.0, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220.0, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440.0, 466.164, 493.883, 523.251, 554.365, 587.330, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880.0, 932.328, 987.767, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.0, 1864.66, 1975.53, 2093.0, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.0, 3729.31, 3951.07, 4186.01};
--- bbb/meta-bbb/recipes-core/trebletrouble/files/src/tone.c
+++ bbb/meta-bbb/recipes-core/trebletrouble/files/src/tone.c
@@ -1,3 +1,32 @@
+/*
+ *  Copyright (C) 2017       William Pearson
+ *  Copyright (C) 2016-2017  Tejan Chinnakonda
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7
+ *
+ *  If you modify this Program, or any covered work, by linking or
+ *  combining it with the uGFX Library, containing parts covered by the
+ *  terms of the GFX License, Version 1.2, the licensors of this Program
+ *  grant you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of the uGFX Library used
+ *  as well as that of the covered work.
+ *
+ */
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <malloc.h>
